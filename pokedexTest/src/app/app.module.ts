cb094723118d632284422e000e4958eb924ec042
mssilva20 <mahteus10170@gmail.com>
2021-07-29 15:09:48 -0300

    primeiro commit

--- pokedexTest/src/app/app.module.ts
+++ pokedexTest/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PokemonListaComponent } from './pokemon-lista/pokemon-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListaComponent
   ],
   imports: [
     BrowserModule
---
+++ pokedexTest/src/app/pokemon-lista/pokemon-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-lista',
+  templateUrl: './pokemon-lista.component.html',
+  styleUrls: ['./pokemon-lista.component.css']
+})
+export class PokemonListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
